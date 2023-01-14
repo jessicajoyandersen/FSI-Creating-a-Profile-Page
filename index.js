2b24112f5b26b7c2a3f8737f4074e27534df01ea
Jessica Andersen <jessicajoyandersen@gmail.com>
2023-01-14 00:53:24 -0500

    duplicated all elements, but all misaligned

--- index.js
+++ index.js
@@ -2,21 +2,41 @@
 let content = document.querySelector('.js-generated.content')
 
 let header = document.createElement('h1')
-header.setAttribute('class', 'dog-name')
+header.setAttribute('class','dog-name')
 header.append('Rizzo')
 content.append(header)
 
-let dogContent = document.createElement('div')
-dogContent.setAttribute('class', 'dog-content')
-content.append(dogContent)
+let image = document.createElement('img')
+image.setAttribute('class','dog-image')
+image.src = './assets/rizzo.jpg'
+content.append(image)
 
-let dogImage = document.createElement('img')
-dogImage.setAttribute('class', 'dog-image')
-dogImage.setAttribute('src', './assets/rizzo.jpg')
-content.append(dogImage)
+let desc = document.createElement('h3')
+desc.setAttribute('class','dog-details')
+desc.append('Description:')
+content.append(desc)
 
-let dogDetails = document.createElement('div')
-dogDetails.setAttribute('class', 'dog-details')
-content.append(dogDetails)
+let par = document.createElement('p')
+par.setAttribute('class','dog-details')
+par.append("This gentle dog is aloof toward her owner, and never comes when called. She always acts as though any stranger she meets will harm her, and dislikes other animals.")
+content.append(par)
 
+let listName = document.createElement('h3')
+listName.setAttribute('class','dog-details')
+listName.append('Feeding Times:')
+content.append(listName)
 
+let listItem1 = document.createElement('li')
+listItem1.setAttribute('class','ul')
+listItem1.append('9:00 am')
+content.append(listItem1)
+
+let listItem2 = document.createElement('li')
+listItem2.setAttribute('class','ul')
+listItem2.append('12:00 pm')
+content.append(listItem2)
+
+let listItem3 = document.createElement('li')
+listItem3.setAttribute('class','ul')
+listItem3.append('12:00 pm')
+content.append(listItem3)
